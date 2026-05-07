1bcaca70e0a16bba4a331bcde313d7003c4e5f14
nekit122 <rna7641@gmail.com>
2026-05-07 19:26:28 +0700

    app.py

--- socialsite/app.py
+++ socialsite/app.py
@@ -575,3 +575,6 @@ with app.app_context():
 if __name__ == '__main__':
     port = int(os.environ.get('PORT', 5000))
     app.run(host='0.0.0.0', port=port, debug=False)
+else:
+    # Для gunicorn на Render
+    port = int(os.environ.get('PORT', 5000))
